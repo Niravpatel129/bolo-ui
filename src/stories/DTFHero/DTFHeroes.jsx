400c78c89f1ae2bb7dcfb4985e85f12c8824be92
Nirav <niravpatelp129@gmail.com>
2024-05-30 18:00:27 -0400

    2

--- src/stories/DTFHero/DTFHeroes.jsx
+++ src/stories/DTFHero/DTFHeroes.jsx
@@ -3,8 +3,8 @@ export default function Template() {
     <div className='bg-[#EBF1F7]'>
       <div className='flex justify-between p-8 gap-28 w-full items-center flex-col md:flex-row'>
         <div className='w-full flex flex-col justify-between items-center max-w-[600px]'>
-          <div className='text-4xl font-bold mb-2'>Custom DTF Transfers straight to your door</div>
-          <div className='text-md my-8'>
+          <div className='text-5xl font-bold mb-2'>Custom DTF Transfers straight to your door</div>
+          <div className='text-xl my-8'>
             Print your design on any product, material, or color with incredible durability and
             vibrancy
           </div>
@@ -22,7 +22,7 @@ export default function Template() {
                   <path strokeLinecap='round' strokeLinejoin='round' d='m4.5 12.75 6 6 9-13.5' />
                 </svg>
               </div>
-              <div className='text-sm text-nowrap'>High Quality</div>
+              <div className='text-lg text-nowrap'>High Quality</div>
             </div>
             <div className='flex gap-2 justify-center items-center'>
               <div>
@@ -37,7 +37,7 @@ export default function Template() {
                   <path strokeLinecap='round' strokeLinejoin='round' d='m4.5 12.75 6 6 9-13.5' />
                 </svg>
               </div>
-              <div className='text-sm text-nowrap'>High Quality</div>
+              <div className='text-lg text-nowrap'>High Quality</div>
             </div>
             <div className='flex gap-2 justify-center items-center'>
               <div>
@@ -52,14 +52,14 @@ export default function Template() {
                   <path strokeLinecap='round' strokeLinejoin='round' d='m4.5 12.75 6 6 9-13.5' />
                 </svg>
               </div>
-              <div className='text-sm text-nowrap'>High Quality</div>
+              <div className='text-lg text-nowrap'>High Quality</div>
             </div>
           </div>
           <div className='mt-8 w-full flex flex-col gap-4'>
-            <div className='w-full text-center bg-[#0176C3] rounded-full py-3 text-white cursor-pointer'>
+            <div className='w-full text-center bg-[#0176C3] rounded-full py-3 text-white cursor-pointer hover:opacity-85'>
               Button 1
             </div>
-            <div className='w-full text-center bg-[#FFFFFF] rounded-full py-3 text-black cursor-pointer border-[1px] border-[#000000]'>
+            <div className='w-full text-center  rounded-full py-3 text-black cursor-pointer border-[1px] border-[#000000] hover:bg-[#0176C3] hover:text-white'>
               Button 1
             </div>
           </div>
