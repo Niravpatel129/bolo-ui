2c457a98e65069f50aec7449f0c7dd64031453af
Nirav <niravpatelp129@gmail.com>
2024-05-30 16:49:04 -0400

    2

--- src/stories/Template/Template.stories.jsx
+++ src/stories/Template/Template.stories.jsx
@@ -1,6 +1,5 @@
 import Template from './Template';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
   title: 'Example/Template',
   component: Template,
@@ -9,7 +8,6 @@ export default {
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default = {
   args: {},
 };
