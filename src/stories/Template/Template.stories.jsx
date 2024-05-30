e90778e6cd3f2f3b856b6e338a426e908d210ade
Nirav <niravpatelp129@gmail.com>
2024-05-30 16:48:53 -0400

    2

--- src/stories/Template/Template.stories.jsx
+++ src/stories/Template/Template.stories.jsx
@@ -5,11 +5,8 @@ export default {
   title: 'Example/Template',
   component: Template,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
