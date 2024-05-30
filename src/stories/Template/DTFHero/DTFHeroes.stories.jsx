006417cfb1a299e554b58269fbcafbeaac62a815
disuhitarth <disuhitarth@gmail.com>
2024-05-30 17:29:37 -0400

    2

---
+++ src/stories/Template/DTFHero/DTFHeroes.stories.jsx
@@ -0,0 +1,13 @@
+import DTFHero from './DTFHeroes';
+
+export default {
+  title: 'Components/DTFHero',
+  component: DTFHero,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Default = {
+  args: {},
+};
