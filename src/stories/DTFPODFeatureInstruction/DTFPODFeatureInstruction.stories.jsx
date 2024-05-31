6549aabe57c95305dd41655eef53d61162bc9b7e
disuhitarth <disuhitarth@gmail.com>
2024-05-31 16:14:03 -0400

    final commit

---
+++ src/stories/DTFPODFeatureInstruction/DTFPODFeatureInstruction.stories.jsx
@@ -0,0 +1,15 @@
+import DTFPODFeatureInstruction from './DTFPODFeatureInstruction';
+
+export default {
+  title: 'Components/DTFPODFeatureInstruction',
+  component: DTFPODFeatureInstruction,
+  parameters: {
+    // layout: 'fullscreen',
+    layout: 'centered',
+  },
+};
+
+export const Default = {
+  args: {},
+};
+// This is comment
