6451c128b0e49c98d31189a9d2dca98a0adc945b
disuhitarth <disuhitarth@gmail.com>
2024-05-30 21:11:29 -0400

    material Added

---
+++ src/stories/MaterialsSelectorComponent/MaterialsSelector.stories.jsx
@@ -0,0 +1,14 @@
+import MaterialsSelectorComponent from './MaterialsSelector';
+
+export default {
+  title: 'Components/MaterialsSelectorComponent',
+  component: MaterialsSelectorComponent,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Default = {
+  args: {},
+};
+// This is comment
