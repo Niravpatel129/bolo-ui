5a89b2ff6da5500a10c55bcbff41e28399043810
disuhitarth <disuhitarth@gmail.com>
2024-05-31 00:01:10 -0400

    All components updated now

---
+++ src/stories/DTFHeroBanner/DTFHeroBanner.stories.jsx
@@ -0,0 +1,13 @@
+import DTFHeroBanner from './DTFHeroBanner';
+
+export default {
+  title: 'Components/DTFHeroBanner',
+  component: DTFHeroBanner,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Default = {
+  args: {},
+};
